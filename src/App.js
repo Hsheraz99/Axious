19af636b4301e670ea8e7a798bb33b3eedf2dcc3
Hafiz Sheraz Hanif Khan <[200~hafizsherazhanif@gmail.com~>
2022-12-11 19:46:55 +0500

    work do

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 function App() {
+
+const [myData,setMyData]=useState([])  
+
+useEffect(() => {
+  axios.get("https://jsonplaceholder.typicode.com/posts").then((res)=> setMyData(res))
+  
+}, [])
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Axious</h1>
+    {myData&&myData.map((posts)=>{
+    const { id,title,body}= posts;
+     return(
+     <div className="card" key={id}>
+      <h2>{title}</h2>
+     <p>{body}</p>
+     </div>
+     )
+    })
+}
+</>
   );
 }
 
